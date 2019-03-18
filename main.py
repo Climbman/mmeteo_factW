28f555dfda03bfa1cd884f14565f9127b4a41bc5
Laurynas <laurynas.kr@hotmail.com>
2019-03-18 20:43:42 +0200

    mods to main.py, configured to work with string input (module)

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ import json
 import classes as cl
 
 def verify(file_name):
-    if isinstance(file_name, str):
-        if len(file_name) == 20 and file_name[-5:] == ".json" and file_name[0:3] == "wm_":
-            return 1
-    ###
-    print("Bad filename: " + file_name)
-    return 0
+    if not isinstance(file_name, str):
+        print("Bad filename: " + file_name)
+        return 0
+        
+    if not (len(file_name) == 20 and file_name[-5:] == ".json" and file_name[0:3] == "wm_"):
+        print("Bad filename: " + file_name)
+        return 0
+        
+    return 1
 
 
 
-def main(file_name):
-    if not verify(file_name):
-        sys.exit()
-    ###
-    #print("The file name is: " + file_name)
+def from_file(file_name):
     
     year = file_name[3:7]
     month = file_name[7:9]
@@ -28,31 +27,52 @@ def main(file_name):
     
     time = [year, month, day, hour]
     
-    
     with open(file_name,'r',encoding = 'utf-8') as json_file:
         json_string = json_file.read()
         
     json_dict = json.loads(json_string)
     
+    create_obj(json_dict, time)
+    
+    
+    
+def from_string(string, time):
+    
+    #TODO: add date verification
+    year = time[0:4]
+    month = time[4:6]
+    day = time[6:8]
+    hour = time[8:10]
+    
+    time = [year, month, day, hour]
+    
+    json_dict = json.loads(string)
+    
+    create_obj(json_dict, time)
+    
+    
+    
+def create_obj(json_dict, time):
+    
+    city_dict = {}
     for station_id, info_block in json_dict.items():
-        
-        city = cl.factWeatherCity(station_id, info_block, time)
-        # print(city.stn_name)
-        # print(city.cond_code)
-        # print(city.cond_txt)
-        # print(city.temp)
-        # print(city.press)
-        # print(city.wind_dir)
-        # print(city.wind_gust)
-        # print(city.datetime)
+        city_dict.update({str(station_id) : cl.factWeatherCity(station_id, info_block, time)})
+    
+    print(city_dict)
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Not enough args")
         sys.exit()
     elif len(sys.argv) > 2:
-        print("Not enough args")
+        print("Too many args")
         sys.exit()
         
-    main(sys.argv[1])
+    if not verify(sys.argv[1]):
+        sys.exit()
+    
+        
+    from_file(sys.argv[1])
     
